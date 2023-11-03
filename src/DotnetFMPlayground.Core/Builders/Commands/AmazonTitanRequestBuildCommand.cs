73bb064289f5a9c30745c9766bbd1e1458b587d2
François Bouteruche <bouteru@amazon.com>
2023-11-03 16:41:46 +0100

    Add support for Amazon Titan streaming response Improve support of Claude, Titan and Stable Diffusion request parameters

---
+++ src/DotnetFMPlayground.Core/Builders/Commands/AmazonTitanRequestBuildCommand.cs
@@ -0,0 +1,62 @@
+﻿using Amazon.BedrockRuntime.Model;
+using DotnetFMPlayground.Core.Models;
+using DotnetFMPlayground.Core.Models.InferenceParameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DotnetFMPlayground.Core.Builders.Commands
+{
+    internal class AmazonTitanRequestBuildCommand : InvokeModelRequestBuildCommand
+    {
+        internal override InvokeModelRequest Execute(string modelId, Prompt prompt, BaseInferenceParameters inferenceParameters)
+        {
+            InvokeModelRequest request = new()
+            {
+                ModelId = modelId,
+                ContentType = "application/json"
+            };
+
+            request.Accept = "application/json";
+            request.Body = new MemoryStream(
+                Encoding.UTF8.GetBytes(
+                    JsonSerializer.Serialize(new
+                    {
+                        inputText = new StringBuilder().AppendJoin(' ', prompt.Select(x => x.Prompt)).ToString()
+                    })
+                )
+            );
+
+            return request;
+        }
+
+        internal override InvokeModelWithResponseStreamRequest ExecuteWithResponseStream(string modelId, Prompt prompt, BaseInferenceParameters inferenceParameters)
+        {
+            if(modelId == "amazon.titan-embed-text-v1")
+            {
+                throw new NotSupportedException($"ModelId {modelId} doesn't support streaming");
+            }
+
+            InvokeModelWithResponseStreamRequest request = new()
+            {
+                ModelId = modelId,
+                ContentType = "application/json"
+            };
+
+            request.Accept = "application/json";
+            request.Body = new MemoryStream(
+                Encoding.UTF8.GetBytes(
+                    JsonSerializer.Serialize(new
+                    {
+                        inputText = new StringBuilder().AppendJoin(' ', prompt.Select(x => x.Prompt)).ToString()
+                    })
+                )
+            );
+
+            return request;
+        }
+    }
+}
---
+++ src/DotnetFMPlayground.Core/Builders/Commands/ClaudeRequestBuildCommand.cs
@@ -0,0 +1,94 @@
+﻿using Amazon.BedrockRuntime.Model;
+using Amazon.Runtime.Internal;
+using DotnetFMPlayground.Core.Models;
+using DotnetFMPlayground.Core.Models.InferenceParameters;
+using System.Text;
+using System.Text.Json;
+
+namespace DotnetFMPlayground.Core.Builders.Commands
+{
+    internal class ClaudeRequestBuildCommand : InvokeModelRequestBuildCommand
+    {
+        internal override InvokeModelRequest Execute(string modelId, Prompt prompt, BaseInferenceParameters inferenceParameters)
+        {
+            InvokeModelRequest request = new()
+            {
+                ModelId = modelId,
+                ContentType = "application/json"
+            };
+
+            StringBuilder promtValueBuilder = new();
+            PromptItemType? lastPromptType = null;
+            foreach (var item in prompt)
+            {
+                lastPromptType = item.Type;
+                string label = item.Type == PromptItemType.User ? "Human" : "Assistant";
+                promtValueBuilder.Append(label);
+                promtValueBuilder.Append(": ");
+                promtValueBuilder.Append(item.Prompt);
+                promtValueBuilder.AppendLine();
+            }
+            if (lastPromptType != PromptItemType.FMAnswer)
+            {
+                if (!promtValueBuilder.ToString().EndsWith('.'))
+                    promtValueBuilder.Append('.');
+                promtValueBuilder.AppendLine();
+                promtValueBuilder.AppendLine("Assistant: ");
+            }
+
+            request.Accept = "application/json";
+            request.Body = new MemoryStream(
+                Encoding.UTF8.GetBytes(
+                    JsonSerializer.Serialize(new
+                    {
+                        prompt = promtValueBuilder.ToString(),
+                        max_tokens_to_sample = inferenceParameters["max_tokens_to_sample"]
+                    })
+                )
+            );
+
+            return request;
+        }
+
+        internal override InvokeModelWithResponseStreamRequest ExecuteWithResponseStream(string modelId, Prompt prompt, BaseInferenceParameters inferenceParameters)
+        {
+            InvokeModelWithResponseStreamRequest request = new()
+            {
+                ModelId = modelId,
+                ContentType = "application/json"
+            };
+
+            StringBuilder promtValueBuilder = new();
+            PromptItemType? lastPromptType = null;
+            foreach (var item in prompt)
+            {
+                lastPromptType = item.Type;
+                string label = item.Type == PromptItemType.User ? "Human" : "Assistant";
+                promtValueBuilder.Append(label);
+                promtValueBuilder.Append(": ");
+                promtValueBuilder.Append(item.Prompt);
+                promtValueBuilder.AppendLine();
+            }
+            if (lastPromptType != PromptItemType.FMAnswer)
+            {
+                if (!promtValueBuilder.ToString().EndsWith('.'))
+                    promtValueBuilder.Append('.');
+                promtValueBuilder.AppendLine();
+                promtValueBuilder.AppendLine("Assistant: ");
+            }
+
+            request.Accept = "application/json";
+            request.Body = new MemoryStream(
+                Encoding.UTF8.GetBytes(
+                    JsonSerializer.Serialize(new
+                    {
+                        prompt = promtValueBuilder.ToString(),
+                        max_tokens_to_sample = inferenceParameters["max_tokens_to_sample"]
+                    })
+                )
+            );
+
+            return request;
+        }
+    }
+}
