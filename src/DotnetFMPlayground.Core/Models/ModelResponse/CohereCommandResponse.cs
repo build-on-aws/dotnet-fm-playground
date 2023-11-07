54d1d2967638da03273212896bf0355db584ee69
François Bouteruche <bouteru@amazon.com>
2023-11-07 18:29:50 +0100

    Change the swtich cases to use the ModelIds constants. Add support for Cohere and Jurassic responses.

---
+++ src/DotnetFMPlayground.Core/Models/ModelResponse/CohereCommandResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetFMPlayground.Core.Models.ModelResponse
+{
+    public class CohereCommandResponse : IFoundationModelResponse
+    {
+        public class CohereCommandGeneration
+        {
+            public int TokenCount { get; init; }
+
+            public string? Text { get; init; }
+
+            public string? Id { get; init; }
+
+            public string? Finish_Reason { get; init; }
+        }
+
+        public IEnumerable<CohereCommandGeneration>? generations { get; set; }
+
+        public string? Id { get; init; }
+
+        public string? Prompt { get; init; }
+
+        public string? GetResponse()
+        {
+            return generations?.FirstOrDefault()?.Text;
+        }
+
+        public string? GetStopReason()
+        {
+            return generations?.FirstOrDefault()?.Finish_Reason;
+        }
+    }
+}
