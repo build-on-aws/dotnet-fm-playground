e0f4b981f5642c2d07da39ba2e7209a367e35302
François Bouteruche <bouteru@amazon.fr>
2023-11-08 18:19:01 +0100

    Complete Bedrock Agent API call

--- src/DotnetFMPlayground.Agent/AmazonBedrockAgentRuntimeClient.cs
+++ src/DotnetFMPlayground.Agent/AmazonBedrockAgentRuntimeClient.cs
@@ -1,21 +1,69 @@
 ﻿using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.Runtime;
+using AwsSignatureVersion4;
+using Amazon.Runtime.Internal.Util;
 
 namespace DotnetFMPlayground.Agent
 {
     public class AmazonBedrockAgentRuntimeClient
     {
+        public class ChunkMessage
+        {
+            public string? Bytes { get; init; }
+        }
 
         private HttpClient httpClient;
 
-        public AmazonBedrockAgentRuntimeClient(HttpClient httpClient)
+        public AmazonBedrockAgentRuntimeClient()
         {
-            this.httpClient = httpClient;
+            var chain = new CredentialProfileStoreChain();
+            AWSCredentials awsCredentials;
+            if (chain.TryGetAWSCredentials("default", out awsCredentials))
+            {
+                var signatureHandler = new AwsSignatureHandler(new AwsSignatureHandlerSettings("us-east-1", "bedrock", awsCredentials))
+                {
+                    InnerHandler = new HttpClientHandler()
+                };
+
+                var httpClient = new HttpClient(signatureHandler)
+                {
+                    BaseAddress = new Uri("https://bedrock-agent-runtime.us-east-1.amazonaws.com")
+                };
+                this.httpClient = httpClient;
+            }
+            else
+            {
+                throw new Exception("missing aws credentials");
+            }
         }
 
-        public async string InvokeAgent(string agentId, string agentAliasId, string inputText)
+        public async Task<string> InvokeAgent(string agentId, string agentAliasId, string inputText)
         {
-            JsonContent.Create();
-            httpClient.PostAsync($"",)
+            Guid sessionId = new Guid();
+            var content = JsonContent.Create( new { inputText });
+            var response = await httpClient.PostAsync($"/agents/{agentId}/agentAliases/{agentAliasId}/sessions/{sessionId.ToString()}/text", content);
+
+            response.EnsureSuccessStatusCode();
+
+
+            var buffer = await response.Content.ReadAsByteArrayAsync();
+            var message = Amazon.Runtime.EventStreams.EventStreamMessage.FromBuffer(buffer, 0, buffer.Length);
+
+            var chunk = JsonSerializer.Deserialize<ChunkMessage>(new MemoryStream(message.Payload), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            if (chunk.Bytes is not null)
+            {
+                var internalObject = Convert.FromBase64String(chunk.Bytes!);
+                var reader = new StreamReader(new MemoryStream(internalObject));
+                var read = await reader.ReadToEndAsync();
+                return read;
+            }
+            else
+            {
+                return "Failure";
+            }
         }
 
     }
