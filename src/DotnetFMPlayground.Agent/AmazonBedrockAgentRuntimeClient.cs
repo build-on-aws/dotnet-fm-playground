ae16e03f25fc1e65a0a1fc2d16d9c722486165f7
François Bouteruche <bouteru@amazon.fr>
2023-11-16 08:44:38 +0100

    Implement the logic to handle the agent response stream. Transform the agent playground into a chat-like playground. The discussion context is hold by the agent not by the client.

--- src/DotnetFMPlayground.Agent/AmazonBedrockAgentRuntimeClient.cs
+++ src/DotnetFMPlayground.Agent/AmazonBedrockAgentRuntimeClient.cs
@@ -1,10 +1,11 @@
-﻿using System.Net.Http.Json;
-using System.Text.Json;
-using System.Text;
+﻿using Amazon.Runtime;
 using Amazon.Runtime.CredentialManagement;
-using Amazon.Runtime;
+using Amazon.Runtime.EventStreams;
+using Amazon.Runtime.EventStreams.Internal;
 using AwsSignatureVersion4;
-using Amazon.Runtime.Internal.Util;
+using System.Net.Http.Json;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
 
 namespace DotnetFMPlayground.Agent
 {
@@ -16,6 +17,8 @@ namespace DotnetFMPlayground.Agent
         }
 
         private HttpClient httpClient;
+
+        private bool isProcessing = false;
 
         public AmazonBedrockAgentRuntimeClient()
         {
@@ -38,31 +41,84 @@ namespace DotnetFMPlayground.Agent
             {
                 throw new Exception("missing aws credentials");
             }
+
         }
 
-        public async Task<string> InvokeAgent(string agentId, string agentAliasId, string inputText)
+        public async IAsyncEnumerable<string> InvokeAgent(string agentId, string agentAliasId, string sessionId, string inputText, [EnumeratorCancellation] CancellationToken cancellationToken = default)
         {
-            Guid sessionId = new Guid();
             var content = JsonContent.Create( new { inputText });
-            var response = await httpClient.PostAsync($"/agents/{agentId}/agentAliases/{agentAliasId}/sessions/{sessionId.ToString()}/text", content);
-
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, $"/agents/{agentId}/agentAliases/{agentAliasId}/sessions/{sessionId}/text")
+            {
+                Content = content
+            };
+            var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                .ConfigureAwait(continueOnCapturedContext: false); 
+           
             response.EnsureSuccessStatusCode();
 
+            var streamCancellationTokenSource = new CancellationTokenSource();
+            var streamTaskCompletionSource = new TaskCompletionSource<string>();
 
-            var buffer = await response.Content.ReadAsByteArrayAsync();
-            var message = Amazon.Runtime.EventStreams.EventStreamMessage.FromBuffer(buffer, 0, buffer.Length);
+            EventStreamDecoder decoder = new EventStreamDecoder();
+            decoder.MessageReceived += HandleMessageReceived;
 
-            var chunk = JsonSerializer.Deserialize<ChunkMessage>(new MemoryStream(message.Payload), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-            if (chunk.Bytes is not null)
+            Stream responseStream = await response.Content.ReadAsStreamAsync();
+
+            isProcessing = true;
+            _ = Task.Run(() => Process(decoder, responseStream));
+
+            while (!streamCancellationTokenSource.IsCancellationRequested && isProcessing)
             {
-                var internalObject = Convert.FromBase64String(chunk.Bytes!);
-                var reader = new StreamReader(new MemoryStream(internalObject));
-                var read = await reader.ReadToEndAsync();
-                return read;
+                yield return await streamTaskCompletionSource.Task.WaitAsync(cancellationToken).ConfigureAwait(false);
+                streamTaskCompletionSource = new TaskCompletionSource<string>();
+            }
+
+            async void  HandleMessageReceived(object? sender, EventStreamMessageReceivedEventArgs e)
+            {
+                if (e.Message.Headers[":event-type"].AsString() != "end")
+                {
+                    var chunk = JsonSerializer.Deserialize<ChunkMessage>(new MemoryStream(e.Message.Payload), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                    string read = "";
+                    if (chunk!.Bytes is not null)
+                    {
+                        var internalObject = Convert.FromBase64String(chunk.Bytes!);
+                        var reader = new StreamReader(new MemoryStream(internalObject));
+                        read = await reader.ReadToEndAsync();
+
+                    }
+                    streamTaskCompletionSource.SetResult(read);
+                }
+                else
+                {
+                    decoder.MessageReceived -= HandleMessageReceived;
+                    streamCancellationTokenSource.Cancel();
+                }
+            }
+
+            decoder.MessageReceived -= HandleMessageReceived;
+
+        }
+        private void Process(EventStreamDecoder decoder, Stream stream)
+        {
+            byte[] buffer = new byte[8192];
+            isProcessing = true;
+            while (isProcessing)
+            {
+                ReadFromStream(decoder, stream, buffer);
+            }
+        }
+
+        private void ReadFromStream(EventStreamDecoder decoder, Stream stream, byte[] buffer)
+        {
+            var bytesRead = stream.Read(buffer, 0, buffer.Length);
+            if (bytesRead > 0)
+            {
+                // Decoder raises MessageReceived for every message it encounters.
+                decoder.ProcessData(buffer, 0, bytesRead);
             }
             else
             {
-                return "Failure";
+                isProcessing = false;
             }
         }
 
--- src/DotnetFMPlayground.Core/Models/PromptItem.cs
+++ src/DotnetFMPlayground.Core/Models/PromptItem.cs
@@ -6,5 +6,16 @@ using System.Threading.Tasks;
 
 namespace DotnetFMPlayground.Core.Models
 {
-    public record PromptItem(PromptItemType Type, string Prompt);
+    public class PromptItem
+    {
+        public PromptItemType Type { get; }
+        
+        public string Prompt { get; set; }
+
+        public PromptItem(PromptItemType type, string prompt)
+        {
+            Type = type;
+            Prompt = prompt;
+        }
+    }
 }
