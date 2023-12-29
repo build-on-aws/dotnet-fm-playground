0810f303318c10b5b3b0914b6eadfcf8315e8ef9
François Bouteruche <bouteru@amazon.fr>
2023-12-29 12:46:51 +0100

    Refactor the Image playground to use a "one invoke method per model" strategy and add support for Amazon Titan Image Generator G1

--- src/DotnetFMPlayground.Core/AmazonBedrockRuntimeClientExtension.cs
+++ src/DotnetFMPlayground.Core/AmazonBedrockRuntimeClientExtension.cs
@@ -1,10 +1,12 @@
-﻿using Amazon.BedrockRuntime;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using Amazon.BedrockRuntime;
 using Amazon.BedrockRuntime.Model;
+using Amazon.Util;
 using DotnetFMPlayground.Core.Builders;
 using DotnetFMPlayground.Core.Models;
-using System.Net;
-using System.Text.Json;
-using System.Text;
 using DotnetFMPlayground.Core.Models.InferenceParameters;
 using DotnetFMPlayground.Core.Models.ModelResponse;
 
@@ -52,5 +54,72 @@ namespace DotnetFMPlayground.Core
 
             response.Body.StartProcessing();
         }
+        
+        /// <summary>
+        /// Invoke Titan Image Generator G1 for text to image generation
+        /// </summary>
+        /// <param name="client">The Amazon Bedrock Runtime client object</param>
+        /// <param name="textToImageParams">The text to image prompt definition</param>
+        /// <param name="imageGenerationConfig">The image configuration definition. If null, default values will be used</param>
+        /// <returns>The Titan Image Generator G1 response</returns>
+        public static async Task<AmazonTitanImageGeneratorG1Response?> InvokeTitanImageGeneratorG1ForTextToImageAsync(
+            this AmazonBedrockRuntimeClient client, Models.InferenceParameters.AmazonTitanImageGeneratorG1.TextToImageParams textToImageParams, Models.InferenceParameters.AmazonTitanImageGeneratorG1.ImageGenerationConfig? imageGenerationConfig = null)
+        {
+            ArgumentNullException.ThrowIfNull(textToImageParams);
+            JsonSerializerOptions jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+            {
+                WriteIndented = true,
+                Converters =
+                {
+                    new JsonStringEnumConverter<Models.InferenceParameters.AmazonTitanImageGeneratorG1.ImageGenerationConfig.ImageQuality>(JsonNamingPolicy.CamelCase)
+                }
+            };
+
+            JsonObject payload = new JsonObject()
+            {
+                ["taskType"] = "TEXT_IMAGE",
+                ["textToImageParams"] = JsonSerializer.SerializeToNode(textToImageParams, jsonSerializerOptions)
+            };
+
+            if (imageGenerationConfig is not null)
+            {
+                payload.Add("imageGenerationConfig", JsonSerializer.SerializeToNode(imageGenerationConfig, jsonSerializerOptions));
+            }
+
+            InvokeModelResponse response = await client.InvokeModelAsync(new InvokeModelRequest()
+            {
+                 ModelId = ModelIds.AMAZON_TITAN_IMAGE_GENERATOR_v1,
+                 ContentType = "application/json",
+                 Accept = "application/json",
+                 Body = AWSSDKUtils.GenerateMemoryStreamFromString(payload.ToJsonString())
+            });
+            
+            return JsonSerializer.Deserialize<AmazonTitanImageGeneratorG1Response>(response.Body, new JsonSerializerOptions());
+        }
+
+        /// <summary>
+        /// Invoke Stability AI Stable Diffusion XL v1 for text to image generation
+        /// </summary>
+        /// <param name="client">The Amazon Bedrock Runtime client object</param>
+        /// <param name="textToImageParams">The text to image prompt definition</param>
+        /// <returns>The Stability AI Stable Diffusion XL response</returns> 
+        public static async Task<StableDiffusionXLResponse?> InvokeStabilityAIStableDiffusionXLv1ForTextToImageAsync(this AmazonBedrockRuntimeClient client, Models.InferenceParameters.StabilityAIStableDiffusionXL.TextToImageParams textToImageParams)
+        {
+            ArgumentNullException.ThrowIfNull(textToImageParams);
+            Validator.ValidateObject(textToImageParams, new ValidationContext(textToImageParams), true);
+            
+            InvokeModelResponse response = await client.InvokeModelAsync(new InvokeModelRequest()
+            {
+                ModelId = ModelIds.STABILITY_AI_STABLE_DIFFUSION_XL_V0,
+                ContentType = "application/json",
+                Accept = "application/json",
+                Body = new MemoryStream(JsonSerializer.SerializeToUtf8Bytes(textToImageParams))
+            });
+            
+            return JsonSerializer.Deserialize<StableDiffusionXLResponse>(response.Body, new JsonSerializerOptions());
+        }
     }
+    
+    
+        
 }
---
+++ src/DotnetFMPlayground.Core/Models/InferenceParameters/AmazonTitanImageGeneratorG1/TaskType.cs
@@ -0,0 +1,9 @@
+namespace DotnetFMPlayground.Core.Models.InferenceParameters.AmazonTitanImageGeneratorG1;
+
+public enum TaskType
+{
+    TextImage,
+    InPainting,
+    OutPainting,
+    ImageVariation
+}
